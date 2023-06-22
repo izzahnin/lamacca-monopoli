6e6a427a32ed871d8942882cc47022e5381570c6
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-23 00:00:08 +0800

    ganti warna footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full items-center justify-center mx-auto">
-      <footer className="flex justify-center p-4 midnight bg-white shadow md:px-6 md:py-8 dark:bg-gray-800">
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <footer className="flex justify-center p-4 midnight bg-white shadow md:px-6 md:py-8 dark:bg-midnight">
+        <span className="block text-sm text-white sm:text-center dark:text-white">
           © 2023{" "}
           <a href="/https://www.instagram.com/lamaccamonopoli.mks/" target="_blank" className="hover:underline">
             Lamaccamonopoli.mks
