956641f63639d1e903c900ee08d90755db9d065f
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-28 10:07:35 +0800

    ubah bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Home() {
   return (
       <div className="bg-white min-h-screen">
-        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">Selamat Datang di Situs Kami</h1>
-            <p className="mt-4 text-xl text-gray-600">Nikmati pengalaman terbaik bersama kami.</p>
+        <div className="bg-midnight max-w-screen mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="text-center justify-center mx-auto align-middle max-w-4xl">
+            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">Lamacca <span className="text-mandarin">Monopoli</span></h1>
+            <p className="mt-4 text-xl text-white">Meningkatkan Literasi Anak Putus Sekolah Berbasis QR Code dan Paupau RiKadong Sebagai Solusi Pengentasan Buta Aksara Kota Makassar</p>
           </div>
         </div>
       </div>
