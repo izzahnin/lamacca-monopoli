d09205fbcff92503cb990b052b35a093b3f47ffe
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-28 10:08:26 +0800

    tambah tombol ke blok-1&2

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Learn() {
   return (
@@ -7,6 +8,53 @@ function Learn() {
         <div className="text-center">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">Belajar</h1>
           <p className="mt-4 text-xl text-gray-600">Pengenalan Huruf, angka, dongeng</p>
+        </div>
+      </div>
+      {/* sesi list blok */}
+      <div className="flex max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        {/* <h1 className="font-bold text-8xl justify-center text-center text-mandarin">A</h1> */}
+        {/* Balla Lompoa */}
+        <div className="flex flex-col w-fit">
+          <Link to="/belajar/ballalompoa" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Balla Lompoa
+          </Link>
+          <Link to="/belajar/saoraja" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Saoraja
+          </Link>
+          <Link to="/belajar/boyang" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Boyang
+          </Link>
+          <Link to="/belajar/tongkonan" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Tongkonan
+          </Link>
+          <Link to="/belajar/langkanae" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Langkanae
+          </Link>
+          <Link to="/belajar/ballatokajang" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Balla To Kajang
+          </Link>
+        </div>
+
+        {/* ===BLOK-2=== */}
+        <div className="flex flex-col w-fit">
+          <Link to="/belajar/ballalompoa" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Balla Lompoa
+          </Link>
+          <Link to="/belajar/saoraja" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Saoraja
+          </Link>
+          <Link to="/belajar/boyang" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Boyang
+          </Link>
+          <Link to="/belajar/tongkonan" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Tongkonan
+          </Link>
+          <Link to="/belajar/langkanae" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Langkanae
+          </Link>
+          <Link to="/belajar/ballatokajang" className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
+            Rumah Adat Balla To Kajang
+          </Link>
         </div>
       </div>
     </div>
