0dacd8644cfe798291d4b9ce0e93f8d49268bf75
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-29 00:15:09 +0800

    edit gap

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  // const [isOpen, setIsOpen] = useState(false);
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
@@ -14,16 +13,12 @@ const Navbar = () => {
     <nav className="z-40 sticky top-0  w-full bg-midnight">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+          <div className="flex items-center gap-6">
             <div className="flex-shrink-0">
-              {/* <img className="h-8 w-8" src="/logo.svg" alt="Logo" /> */}
               <h1 className="text-white px-2 rounded-md font-bold">Lamacca <span className="text-mandarin">Monopoli</span></h1>
             </div>
             <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                {/* <a href="/home" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                  Beranda
-                </a> */}
+              <div className="flex items-baseline gap-4">
                 <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                   Beranda
                 </Link>
@@ -33,9 +28,6 @@ const Navbar = () => {
                 <Link to="/belajar" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                   Belajar
                 </Link>
-                {/*<a href="/#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                  Contact
-                </a>*/}
               </div>
             </div>
           </div>
@@ -59,7 +51,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className={`${isOpen ? "block" : "hidden"} md:hidden`} id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <div className="flex flex-col gap-1 px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Beranda
           </Link>
@@ -69,10 +61,6 @@ const Navbar = () => {
           <Link to="/belajar" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Belajar
           </Link>
-
-          {/* <Link to="//#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-            Contact
-          </Link> */}
         </div>
       </div>
     </nav>
