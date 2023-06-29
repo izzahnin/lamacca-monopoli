6953d590b604091e26712ff2bacd29e7834eb51c
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-29 17:37:04 +0800

    add props.color

--- src/components/Sublearn/index.jsx
+++ src/components/Sublearn/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function Sublearn(props) {
+  const combinedClassName = `p-1 text-center w-40 sm:w-36 md:w-44 lg:w-60 h-20  rounded-lg flex items-center justify-center text-white  ${props.color}`;
+
   return (
     <>
-      <Link to={props.path} className="w-60 h-20 bg-mandarin rounded-lg flex items-center justify-center text-white">
-        <h1>
-          {props.text}
-        </h1>
+      <Link to={props.path} className={combinedClassName}>
+        <h1>{props.text}</h1>
       </Link>
     </>
   );
