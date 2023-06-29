4a620a2552a46f4837b4e7c2e60096fbb2ec8bb9
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-30 00:00:16 +0800

    add shadow

--- src/components/Sublearn/index.jsx
+++ src/components/Sublearn/index.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 function Sublearn(props) {
-  const combinedClassName = `p-1 text-center w-40 sm:w-36 md:w-44 lg:w-60 h-20  rounded-lg flex items-center justify-center text-white  ${props.color}`;
+  const combinedClassName = `p-1 text-center w-40 sm:w-36 md:w-44 lg:w-60 h-20 shadowbox rounded-lg flex items-center justify-center text-white hover:scale-105 hover:font-bold ${props.color}`;
 
   return (
     <>
       <Link to={props.path} className={combinedClassName}>
-        <h1>{props.text}</h1>
+        <h1 className="font-semibold">{props.text}</h1>
       </Link>
     </>
   );
