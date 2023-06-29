c02e26c2421c0d81024529eeab1f8c2287d796e3
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-29 17:37:26 +0800

    add color bg

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -10,46 +10,50 @@ function Learn() {
           <p className="text-xl text-gray-600">Membaca dan Mengenal Angka & Huruf</p>
         </div>
         <div className="flex justify-center gap-1">
-          <div className="flex flex-col w-fit gap-1">
-            {/* ===Blok-1=== */}
-            <Sublearn text="Rumah Adat Balla Lompoa" path="/belajar/ballalompoa" />
-            <Sublearn text="Rumah Adat Sauraja" path="/belajar/sauraja" />
-            <Sublearn text="Rumah Adat Boyang" path="/belajar/boyang" />
-            <Sublearn text="Rumah Adat Tongkonan" path="/belajar/tongkonan" />
-            <Sublearn text="Rumah Adat Jangkanae" path="/belajar/jangkanae" />
-            <Sublearn text="Rumah Adat Balla To Kajang" path="/belajar/ballatokajang" />
+          <div className="flex flex-col gap-1 sm:flex-row">
+            <div className="flex flex-col w-fit gap-1">
+              {/* ===Blok-1=== */}
+              <Sublearn color="bg-a" text="Rumah Adat Balla Lompoa" path="/belajar/ballalompoa" />
+              <Sublearn color="bg-a" text="Rumah Adat Sauraja" path="/belajar/sauraja" />
+              <Sublearn color="bg-a" text="Rumah Adat Boyang" path="/belajar/boyang" />
+              <Sublearn color="bg-b" text="Rumah Adat Tongkonan" path="/belajar/tongkonan" />
+              <Sublearn color="bg-b" text="Rumah Adat Jangkanae" path="/belajar/jangkanae" />
+              <Sublearn color="bg-b" text="Rumah Adat Balla To Kajang" path="/belajar/ballatokajang" />
+            </div>
+
+            <div className="flex flex-col w-fit gap-1">
+              {/* ===Blok-2=== */}
+              <Sublearn color="bg-c" text="Kapurung" path="/belajar/kapurung" />
+              <Sublearn color="bg-c" text="Es Pisang Ijo" path="/belajar/espisangijo" />
+              <Sublearn color="bg-c" text="Buras" path="/belajar/buras" />
+              <Sublearn color="bg-d" text="Pisang Eppe" path="/belajar/pisangeppe" />
+              <Sublearn color="bg-d" text="Palekko" path="/belajar/palekko" />
+              <Sublearn color="bg-d" text="Mie Titi" path="/belajar/mietiti" />
+            </div>
           </div>
 
-          <div className="flex flex-col w-fit gap-1">
-            {/* ===Blok-2=== */}
-            <Sublearn text="Kapurung" path="/belajar/kapurung" />
-            <Sublearn text="Es Pisang Ijo" path="/belajar/espisangijo" />
-            <Sublearn text="Buras" path="/belajar/buras" />
-            <Sublearn text="Pisang Eppe" path="/belajar/pisangeppe" />
-            <Sublearn text="Palekko" path="/belajar/palekko" />
-            <Sublearn text="Mie Titi" path="/belajar/mietiti" />
-            <Sublearn text="Soto Konro" path="/belajar/sotokonro" />
-          </div>
+          <div className="flex flex-col gap-1 sm:flex-row">
+            <div className="flex flex-col w-fit gap-1">
+              {/* ===Blok-3=== */}
+              <Sublearn color="bg-e" text="Soto Konro" path="/belajar/sotokonro" />
+              <Sublearn color="bg-e" text="Jeneponto" path="/belajar/jeneponto" />
+              <Sublearn color="bg-e" text="Bantaeng" path="/belajar/bantaeng" />
+              <Sublearn color="bg-e" text="Luwu" path="/belajar/luwu" />
+              <Sublearn color="bg-f" text="Palopo" path="/belajar/palopo" />
+              <Sublearn color="bg-f" text="Pinrang" path="/belajar/pinrang" />
+              <Sublearn color="bg-f" text="Toraja" path="/belajar/toraja" />
+            </div>
 
-          <div className="flex flex-col w-fit gap-1">
-            {/* ===Blok-3=== */}
-            <Sublearn text="Jeneponto" path="/belajar/jeneponto" />
-            <Sublearn text="Bantaeng" path="/belajar/bantaeng" />
-            <Sublearn text="Luwu" path="/belajar/luwu" />
-            <Sublearn text="Palopo" path="/belajar/palopo" />
-            <Sublearn text="Pinrang" path="/belajar/pinrang" />
-            <Sublearn text="Toraja" path="/belajar/toraja" />
-            <Sublearn text="Wajo" path="/belajar/wajo" />
-          </div>
-
-          <div className="flex flex-col w-fit gap-1">
-            {/* ===Blok-4=== */}
-            <Sublearn text="Baju Pokko" path="/belajar/pokko" />
-            <Sublearn text="Baju Seppa Tallung" path="/belajar/seppatallung" />
-            <Sublearn text="Baju Bodo" path="/belajar/bodo" />
-            <Sublearn text="Baju Bella Dada" path="/belajar/belladada" />
-            <Sublearn text="Baju Pattuqduq Towaine" path="/belajar/pattuqduqtowaine" />
-            <Sublearn text="Baju Labbu" path="/belajar/labbu" />
+            <div className="flex flex-col w-fit gap-1">
+              {/* ===Blok-4=== */}
+              <Sublearn color="bg-g" text="Wajo" path="/belajar/wajo" />
+              <Sublearn color="bg-g" text="Baju Pokko" path="/belajar/pokko" />
+              <Sublearn color="bg-g" text="Baju Seppa Tallung" path="/belajar/seppatallung" />
+              <Sublearn color="bg-h" text="Baju Bodo" path="/belajar/bodo" />
+              <Sublearn color="bg-h" text="Baju Bella Dada" path="/belajar/belladada" />
+              <Sublearn color="bg-h" text="Baju Pattuqduq Towaine" path="/belajar/pattuqduqtowaine" />
+              <Sublearn color="bg-h" text="Baju Labbu" path="/belajar/labbu" />
+            </div>
           </div>
         </div>
       </div>
