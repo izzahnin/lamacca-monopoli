273200fee67b6bb6831e1380f611225173a2bb47
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-02 00:51:51 +0800

    add paragparh componen

---
+++ src/components/Paragraph/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Paragraph(props) {
+  return (
+    <>
+      <p className="pt-3 text-neutral-900">{props.par}</p>
+    </>
+  );
+}
+
+export default Paragraph;
