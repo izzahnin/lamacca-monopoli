ee28a7474f9c44820a07d5dcf8cee73b6f92d7dd
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-01 17:46:40 +0800

    monopoli image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import "./global.css";
 function Home() {
   return (
     <div className="bg-white min-h-fit">
-      <div className="flex flex-col bg-white max-w-screen mx-auto py-16 px-4 sm:px-6 lg:px-8 gap-20">
+      <div className="flex flex-col bg-white max-w-screen mx-auto py-14 px-4 sm:px-6 lg:px-8 gap-20">
         <div className="text-center justify-center mx-auto align-middle max-w-4xl">
           <h1 className="text-4xl font-extrabold text-navy sm:text-5xl md:text-6xl">
             Lamacca <span className="text-aqua">Monopoli</span>
@@ -12,8 +12,11 @@ function Home() {
           <p className="mt-4 text-xl text-navy">Meningkatkan Literasi Anak Putus Sekolah Berbasis QR Code dan Paupau RiKadong Sebagai Solusi Pengentasan Buta Aksara Kota Makassar</p>
         </div>
       </div>
-      <div className="flex justify-center bg-navy max-w-screen text-white">
+      <div className="flex flex-col sm:flex-row justify-center items-center align-middle bg-navy max-w-screen h-fit p-5 text-white gap-5">
         <img className="w-72" src={process.env.PUBLIC_URL + "/images/animasi2.svg"} alt="" />
+        <img className="w-96" src={process.env.PUBLIC_URL + "/images/monopoli.png"} alt="" />
+      </div>
+      <div>
         
       </div>
     </div>
