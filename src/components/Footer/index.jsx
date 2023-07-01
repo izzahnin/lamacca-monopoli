1177307757c65d3e1ea9017f596591d1291fc000
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-01 14:57:47 +0800

    footer padding

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full items-center justify-center mx-auto ">
-      <div className="flex  bg-sky-100 items-center align-middle justify-center py-9 px-3">
+      <div className="flex  bg-sky-100 items-center align-middle justify-center py-8 px-3">
         <div className="flex items-center gap-3">
-          <img src={process.env.PUBLIC_URL + "/images/logo1.png"} alt="Logo Unhas" className="h-10 w-auto" />
-          <img src={process.env.PUBLIC_URL + "/images/logo2.png"} alt="Logo Kemendikbud" className="h-10 w-auto" />
-          <img src={process.env.PUBLIC_URL + "/images/logo3.png"} alt="Logo Kampus Merdeka" className="h-10 w-auto" />
-          <img src={process.env.PUBLIC_URL + "/images/logo4.png"} alt="Logo PKM" className="h-10 w-auto" />
-          <img src={process.env.PUBLIC_URL + "/images/logo5.png"} alt="Logo Simbelmawa" className="h-10 w-auto" />
+          <img src={process.env.PUBLIC_URL + "/images/logo1.png"} alt="Logo Unhas" className="h-9 sm:h-10 w-auto" />
+          <img src={process.env.PUBLIC_URL + "/images/logo2.png"} alt="Logo Kemendikbud" className="h-9 sm:h-10 w-auto" />
+          <img src={process.env.PUBLIC_URL + "/images/logo3.png"} alt="Logo Kampus Merdeka" className="h-9 sm:h-10 w-auto" />
+          <img src={process.env.PUBLIC_URL + "/images/logo4.png"} alt="Logo PKM" className="h-9 sm:h-10 w-auto" />
+          <img src={process.env.PUBLIC_URL + "/images/logo5.png"} alt="Logo Simbelmawa" className="h-9 sm:h-10 w-auto" />
         </div>
       </div>
       <footer className="flex align-middle items-center justify-center p-4 midnight bg-white shadow md:px-6 md:py-8 dark:bg-navy">
