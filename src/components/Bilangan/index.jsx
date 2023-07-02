1ead6faaa39d21d633411d360911502ce776d2e7
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-02 15:28:20 +0800

    add bilangan component

---
+++ src/components/Bilangan/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Bilangan(props) {
+  return (
+    <>
+      <div className="flex flex-col justify-center align-middle items-center  h-96 gap-2 p-3 mb-12 m-auto">
+        <img className="h-56" src={process.env.PUBLIC_URL + props.src} alt={props.alt} />
+      </div>
+    </>
+  );
+}
+
+export default Bilangan;
