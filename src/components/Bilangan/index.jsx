5027a1ba380b6140da31f536670a1ef48944fa44
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-02 16:08:08 +0800

    ubah height

--- src/components/Bilangan/index.jsx
+++ src/components/Bilangan/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Bilangan(props) {
   return (
     <>
-      <div className="flex flex-col justify-center align-middle items-center  h-96 gap-2 p-3 mb-12 m-auto">
+      <div className="flex flex-col justify-center align-middle items-center  h-[550px] gap-2 p-3 mb-12 m-auto">
         <img className="h-56" src={process.env.PUBLIC_URL + props.src} alt={props.alt} />
       </div>
     </>
