86f186e7c877971c54a3c1421f61a37eea9ef7e8
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-07-11 14:18:07 +0800

    change height

--- src/components/Bilangan/index.jsx
+++ src/components/Bilangan/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Bilangan(props) {
   return (
-    <div className="bg-cover bg-repeat bg-center flex flex-col justify-center align-middle items-center w-full min-h-[100vh] gap-2 px-3 m-auto" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/images/bglontara.svg"})` }}>
+    <div className="bg-cover bg-repeat bg-center flex flex-col justify-center align-middle items-center w-full min-h-[calc(100vh-64px)] gap-2 px-3 m-auto" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/images/bglontara.svg"})` }}>
       <img className="h-56" src={process.env.PUBLIC_URL + props.src} alt={props.alt} />
     </div>
   );
